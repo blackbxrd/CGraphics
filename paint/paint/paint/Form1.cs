05062e0395e6a67ddaafeacca15c13aa2383b306
blackbxrd <72487241+blackbxrd@users.noreply.github.com>
2023-03-28 15:31:34 +0900

    Add files via upload

--- paint/paint/paint/Form1.cs
+++ paint/paint/paint/Form1.cs
@@ -26,8 +26,7 @@ namespace paint
         Pen p = new Pen(Color.Black, 1);
         int index;
         int x, y, cX, cY, arX, arY;
-        int[,] array4dots;
-        int[,] array4line;
+        
 
 
         public Form1()
@@ -35,8 +34,7 @@ namespace paint
             InitializeComponent();
             this.Width = 900;
             this.Height = 700; 
-            int[,] array4dots = new int[bm.Width, bm.Height];
-            int[,] array4line = new int[bm.Width, bm.Height];
+            
 
 
             bm = new Bitmap(pic.Width, pic.Height);
@@ -79,14 +77,14 @@ namespace paint
         private void button1_Click(object sender, EventArgs e)
         {
             
-            var c = new Point(bm.Width / 2, bm.Height / 2);
-            var temp = new Bitmap(bm);
-            var gr = Graphics.FromImage(bm);
-            gr.Clear(Color.White);
-            int angle = 90;
+          //  var c = new Point(bm.Width / 2, bm.Height / 2);
+          //  var temp = new Bitmap(bm);
+          //  var gr = Graphics.FromImage(bm);
+         //   gr.Clear(Color.White);
+          //  int angle = 90;
 
-            double sin = Math.Sin(angle);
-            double cos = Math.Cos(angle);
+          //  double sin = Math.Sin(angle);
+           // double cos = Math.Cos(angle);
 
 
             
@@ -95,8 +93,8 @@ namespace paint
             
            
                 
-            gr.DrawImage(temp, Point.Empty);
-            
+           // gr.DrawImage(temp, Point.Empty);
+            RotatePixels(45);
 
 
         }
@@ -126,7 +124,7 @@ namespace paint
                     arX = e.X;
                     arY = e.Y;
                     g.DrawLine(p, px, py);
-                    array4dots[arY, arX] = 1;
+                    
                     py = px;
                 }
             }
@@ -157,7 +155,55 @@ namespace paint
 
         }
 
-    
+        private void RotatePixels(double angle)
+        {
+            
+            int width = pic.Width;
+            int height = pic.Height;
+
+            
+            Bitmap rotatedBitmap = new Bitmap(width, height);
+
+            
+            double centerX = width / 2.0;
+            double centerY = height / 2.0;
+
+            
+            double radians = angle * Math.PI / 180.0;
+
+           
+            double cos = Math.Cos(radians);
+            double sin = Math.Sin(radians);
+
+            
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    // Вычисляем координаты пикселя относительно центра PictureBox
+                    double translatedX = x - centerX;
+                    double translatedY = y - centerY;
+
+                    // Вычисляем новые координаты пикселя после поворота
+                    double rotatedX = translatedX * cos - translatedY * sin + centerX;
+                    double rotatedY = translatedX * sin + translatedY * cos + centerY;
+
+                    // Проверяем, что новые координаты находятся в пределах PictureBox
+                    if (rotatedX >= 0 && rotatedX < width && rotatedY >= 0 && rotatedY < height)
+                    {
+                        
+                        Color color = ((Bitmap)pic.Image).GetPixel(x, y);
+
+                        
+                        rotatedBitmap.SetPixel((int)rotatedX, (int)rotatedY, color);
+                    }
+                }
+            }
+
+            
+            pic.Image = rotatedBitmap;
+        }
+
 
     }
 }
