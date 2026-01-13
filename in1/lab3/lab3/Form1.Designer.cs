694a0d5284bb5463adfe7530393f220a5bb10b20
Ari Hatano <72487241+blackbxrd@users.noreply.github.com>
2026-01-14 08:24:35 +0900

    Add files via upload

---
+++ in1/lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.invertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fillToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.linesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.filtersToolStripMenuItem,
+            this.modeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // filtersToolStripMenuItem
+            // 
+            this.filtersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.invertToolStripMenuItem});
+            this.filtersToolStripMenuItem.Name = "filtersToolStripMenuItem";
+            this.filtersToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.filtersToolStripMenuItem.Text = "Filters";
+            // 
+            // invertToolStripMenuItem
+            // 
+            this.invertToolStripMenuItem.Name = "invertToolStripMenuItem";
+            this.invertToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.invertToolStripMenuItem.Text = "Invert";
+            this.invertToolStripMenuItem.Click += new System.EventHandler(this.invertToolStripMenuItem_Click);
+            // 
+            // modeToolStripMenuItem
+            // 
+            this.modeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lineToolStripMenuItem,
+            this.fillToolStripMenuItem,
+            this.linesToolStripMenuItem});
+            this.modeToolStripMenuItem.Name = "modeToolStripMenuItem";
+            this.modeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.modeToolStripMenuItem.Text = "Mode";
+            // 
+            // lineToolStripMenuItem
+            // 
+            this.lineToolStripMenuItem.Name = "lineToolStripMenuItem";
+            this.lineToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.lineToolStripMenuItem.Text = "Line";
+            this.lineToolStripMenuItem.Click += new System.EventHandler(this.lineToolStripMenuItem_Click);
+            // 
+            // fillToolStripMenuItem
+            // 
+            this.fillToolStripMenuItem.Name = "fillToolStripMenuItem";
+            this.fillToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.fillToolStripMenuItem.Text = "Fill";
+            this.fillToolStripMenuItem.Click += new System.EventHandler(this.fillToolStripMenuItem_Click);
+            // 
+            // linesToolStripMenuItem
+            // 
+            this.linesToolStripMenuItem.Name = "linesToolStripMenuItem";
+            this.linesToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.linesToolStripMenuItem.Text = "Lines";
+            this.linesToolStripMenuItem.Click += new System.EventHandler(this.linesToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem filtersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem invertToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripMenuItem modeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fillToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem linesToolStripMenuItem;
+    }
+}
+
---
+++ in1/lab3/lab3/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        
+
+        // Переменные для рисования
+        private Point startPoint;
+        private Point endPoint;
+        private bool isDrawing = false;
+        private Pen drawingPen = new Pen(Color.Black, 2);
+        private Bitmap drawingBitmap;
+       
+
+        // Флаги режимов
+        private bool isLineMode = false;
+        private bool isFillMode = false;
+        private Color fillColor = Color.Red;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Инициализируем Bitmap для рисования
+            drawingBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = drawingBitmap;
+            ClearDrawingCanvas();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void invertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Загрузите изображение сначала!");
+                return;
+            }
+
+            Bitmap originalImage = new Bitmap(pictureBox1.Image);
+            Bitmap invertedImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    Color originalColor = originalImage.GetPixel(x, y);
+                    Color invertedColor = Color.FromArgb(
+                        255 - originalColor.R,
+                        255 - originalColor.G,
+                        255 - originalColor.B
+                    );
+                    invertedImage.SetPixel(x, y, invertedColor);
+                }
+            }
+
+            pictureBox1.Image = invertedImage;
+            drawingBitmap = new Bitmap(invertedImage);
+            originalImage.Dispose();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearDrawingCanvas();
+        }
+
+        private void ClearDrawingCanvas()
+        {
+            using (Graphics g = Graphics.FromImage(drawingBitmap))
+            {
+                g.Clear(Color.White);
+            }
+            pictureBox1.Image = drawingBitmap;
+            pictureBox1.Refresh();
+        }
+
+        private void lineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Активируем режим рисования линий
+            isLineMode = true;
+            isFillMode = false;
+            pictureBox1.Cursor = Cursors.Cross;
+        }
+
+        private void fillToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isFillMode = true;
+            isLineMode = false;
+            pictureBox1.Cursor = Cursors.Arrow;
+        }
+
+        // События мыши для рисования линий
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (isLineMode && e.Button == MouseButtons.Left)
+            {
+                // Начало рисования линии
+                startPoint = e.Location;
+                isDrawing = true;
+            }
+            else if (isFillMode && e.Button == MouseButtons.Left)
+            {
+                // Выполняем заливку
+                FloodFill(e.Location);
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing && isLineMode)
+            {
+                // Рисуем предварительную линию
+                endPoint = e.Location;
+
+                // Создаем временную копию изображения для предварительного просмотра
+                Bitmap tempBitmap = new Bitmap(drawingBitmap);
+
+                // Используем алгоритм Брезенхема для рисования линии
+                DrawBresenhamLine(tempBitmap, startPoint, endPoint, drawingPen.Color);
+
+                pictureBox1.Image = tempBitmap;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isDrawing && isLineMode && e.Button == MouseButtons.Left)
+            {
+                // Завершаем рисование линии
+                endPoint = e.Location;
+
+                // Рисуем финальную линию на основном изображении с помощью алгоритма Брезенхема
+                DrawBresenhamLine(drawingBitmap, startPoint, endPoint, drawingPen.Color);
+
+                pictureBox1.Image = drawingBitmap;
+                pictureBox1.Refresh();
+                isDrawing = false;
+            }
+        }
+
+        // Метод заливки (Flood Fill)
+        private void FloodFill(Point startPoint)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Сначала загрузите изображение или создайте холст!");
+                return;
+            }
+
+            Bitmap bmp = new Bitmap(drawingBitmap);
+            Color targetColor = bmp.GetPixel(startPoint.X, startPoint.Y);
+            Color replacementColor = fillColor;
+
+            // Если цвет уже совпадает с цветом заливки, выходим
+            if (targetColor.ToArgb() == replacementColor.ToArgb())
+                return;
+
+            // Используем стек для реализации алгоритма заливки
+            Stack<Point> pixels = new Stack<Point>();
+            pixels.Push(startPoint);
+
+            while (pixels.Count > 0)
+            {
+                Point pt = pixels.Pop();
+
+                // Проверяем границы
+                if (pt.X < 0 || pt.X >= bmp.Width || pt.Y < 0 || pt.Y >= bmp.Height)
+                    continue;
+
+                // Проверяем цвет пикселя
+                if (bmp.GetPixel(pt.X, pt.Y).ToArgb() != targetColor.ToArgb())
+                    continue;
+
+                // Заменяем цвет
+                bmp.SetPixel(pt.X, pt.Y, replacementColor);
+
+                // Добавляем соседние пиксели в стек
+                pixels.Push(new Point(pt.X - 1, pt.Y)); // слева
+                pixels.Push(new Point(pt.X + 1, pt.Y)); // справа
+                pixels.Push(new Point(pt.X, pt.Y - 1)); // сверху
+                pixels.Push(new Point(pt.X, pt.Y + 1)); // снизу
+            }
+
+            // Обновляем изображение
+            drawingBitmap = bmp;
+            pictureBox1.Image = drawingBitmap;
+            pictureBox1.Refresh();
+        }
+
+        // Алгоритм Брезенхема для рисования линии
+        private void DrawBresenhamLine(Bitmap bitmap, Point p1, Point p2, Color color)
+        {
+            int x0 = p1.X;
+            int y0 = p1.Y;
+            int x1 = p2.X;
+            int y1 = p2.Y;
+
+            int dx = Math.Abs(x1 - x0);
+            int dy = Math.Abs(y1 - y0);
+            int sx = (x0 < x1) ? 1 : -1;
+            int sy = (y0 < y1) ? 1 : -1;
+            int err = dx - dy;
+
+            while (true)
+            {
+                // Проверяем границы изображения
+                if (x0 >= 0 && x0 < bitmap.Width && y0 >= 0 && y0 < bitmap.Height)
+                {
+                    bitmap.SetPixel(x0, y0, color);
+                }
+
+                if (x0 == x1 && y0 == y1)
+                    break;
+
+                int e2 = 2 * err;
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x0 += sx;
+                }
+                if (e2 < dx)
+                {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void linesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
